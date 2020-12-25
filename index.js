ffd7bb8f340851399e73b3960cb5f86c8dcd0439
Thorsten Staender <thorsten.staender@wagestolz.de>
2020-12-25 21:04:40 +0100

    Index.js, db.js, s3.js setup

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-// global imports
-import './src/toggleSidebar.js';
-import './src/cart/toggleCart.js';
-import './src/cart/setupCart.js';
-// specific imports
-import fetchProducts from './src/fetchProducts.js';
-import { setupStore, store } from './src/store.js';
-import display from './src/displayProducts.js';
-import { getElement } from './src/utils.js';
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser'); //
+const db = require('./db');
+const s3 = require('./s3');
+const { s3Url } = require('./config.json');
+
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+if (require.main == module) {
+    app.listen(process.env.PORT || 8080, () => {
+        console.log('Imageboard up and running');
+    });
+}
+
+// // global imports
+// import './src/toggleSidebar.js';
+// import './src/cart/toggleCart.js';
+// import './src/cart/setupCart.js';
+// // specific imports
+// import fetchProducts from './src/fetchProducts.js';
+// import { setupStore, store } from './src/store.js';
+// import display from './src/displayProducts.js';
+// import { getElement } from './src/utils.js';
